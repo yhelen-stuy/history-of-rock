682e7699d557853c076231136acf4e9074393ba9
Helen Ye <00yhelen@gmail.com>
2018-04-11 08:25:14 -0400

    Import json in app

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
+
+f = open("./active-years.json", 'rU')
+bands = json.loads(f.read())
+f.close()
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run()
+    # print bands
+    app.debug = True
+    app.run()
