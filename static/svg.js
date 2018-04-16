9afa729d38ead5461ce4a27e83a3edb0cf97ee23
Karina Ionkina <kionkina@stuy.edu>
2018-04-16 13:14:47 -0400

    timeline

--- static/svg.js
+++ static/svg.js
@@ -117,4 +117,21 @@ var setup = function() {
         .style("opacity", "0.6");
 }
 
+
+var chart = d3.timeline();
+
+var testData = [
+		{class: "pA", label: "person a", times: [
+{"starting_time": 1355752800000, "ending_time": 1355759900000},
+{"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+		{class: "pB", label: "person b", times: [
+{"starting_time": 1355759910000, "ending_time": 1355761900000}]},
+		{class: "pC", label: "person c", times: [
+{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+		]; 
+
+
+svg.attr("width", 500).datum(testData).call(chart);
+
+
 setup();
